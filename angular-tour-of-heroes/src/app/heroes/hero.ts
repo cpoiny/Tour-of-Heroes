feadc19a8cde933afbeb4c1b3b6f0e0e0c6bad8d
Cynthia Poiny <cynthia.poiny@gmail.com>
2023-03-07 17:13:23 +0100

    ajout de l'interface Hero et affichage sur le html

---
+++ angular-tour-of-heroes/src/app/heroes/hero.ts
@@ -0,0 +1,6 @@
+// Creation d'une interface hero avec un id et un nom
+
+export interface Hero {
+    id: number;
+    name : string
+}
--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-heroes',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
+  // ajout d'une propriété héro
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  }
+
 
 }
