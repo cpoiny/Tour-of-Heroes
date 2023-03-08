19b8c1ffa359e0141f650225f6b87c7f200b4931
Cynthia Poiny <cynthia.poiny@gmail.com>
2023-03-08 17:41:43 +0100

    creation du composant app-hero-detail

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -8,5 +8,7 @@ import { Hero } from '../heroes/hero';
 })
 export class HeroDetailComponent {
   // on récupere le Hero du parent (HeroesComponent.ts) qui est associé au selectedHero
+  // hero? signifie que je parle du hero dans le html et on ajoute un ? sinon erreur
+  // Hero est la valeur Hero qui est associé au selectedHero et qui a les propriétés définies dans son interface
 @Input() hero? : Hero;
 }
