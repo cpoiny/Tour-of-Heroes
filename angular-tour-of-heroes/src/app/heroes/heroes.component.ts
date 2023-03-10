65a64dc64de57512baac04761fa12b2d836e5e0c
Cynthia Poiny <cynthia.poiny@gmail.com>
2023-03-10 10:54:02 +0100

    création du service Hero et fonction getHero avec mock puis avec observable

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../service/hero.service';
 import { Hero } from './hero';
 
 @Component({
@@ -8,19 +8,31 @@ import { Hero } from './hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  // ajout d'une propriété héro
-  // hero: Hero = {
-  //   id: 1,
-  //   name: 'Windstorm'
-  // }
-
+ 
   // on recupere le mock HEROES
-  heroes = HEROES;
+  heroes : Hero [] = [];
+  
 
   //fonction pour selectionner les details d'un hero selectionné
   selectedHero?: Hero;
 
+
+constructor (
+  private heroService: HeroService
+) {}
+
+ngOnInit() {
+  this.getHeroes();
+}
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+
+
 }
